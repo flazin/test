0e41db865996e5f5fe986a5f74a1814f8539aa5c
akmaljon <akmaljon@gmail.com>
2024-02-27 21:19:52 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Form from './Form';
+
+
+function App() {
+  return (
+    <div className="app">
+      <div className="main-content-cnt">
+        <div className="main-text">
+          <h1>Learn to code by watching others</h1>
+          <h6>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</h6>
+        </div>
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default App;
